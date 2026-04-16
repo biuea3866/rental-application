3b45d7ed03b22f96037b5071aa93725c908a306c
biuea <biuea3866@gmail.com>
2026-04-16 21:20:26 +0900

    Revert "feat(RC-201/202/211/217): Rental 도메인 + Flyway + 낙관적 락 (#45)"
    This reverts commit 324a10aff13b95061b7cf292a9cb1c5646ec6690.

--- rental-domain/src/main/kotlin/com/rental/commerce/domain/rental/PaymentMethod.kt
+++
@@ -1,8 +0,0 @@
-package com.rental.commerce.domain.rental
-
-enum class PaymentMethod {
-    CARD,
-    BANK_TRANSFER,
-    TOSS_PAY,
-    KAKAO_PAY,
-}
--- rental-domain/src/main/kotlin/com/rental/commerce/domain/rental/RentalEventPublisher.kt
+++
@@ -1,7 +0,0 @@
-package com.rental.commerce.domain.rental
-
-import com.rental.commerce.domain.rental.event.RentalStatusChangedEvent
-
-interface RentalEventPublisher {
-    fun publish(event: RentalStatusChangedEvent)
-}
